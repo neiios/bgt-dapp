290ee6bf8beb0f9dcff17afae491c9da12134efd
Igor <68248740+richard96292@users.noreply.github.com>
2022-12-14 22:03:58 +0200

    Fix responsiveness

--- src/components/InfoContainer.jsx
+++ src/components/InfoContainer.jsx
@@ -1,6 +1,6 @@
 export default function InfoContainer({ heading, value }) {
   return (
-    <div className="grid gap-4 md:grid-cols-2">
+    <div className="grid gap-4 lg:grid-cols-2">
       <h2 className="font-bold">{heading}</h2>
       <p>{value}</p>
     </div>
