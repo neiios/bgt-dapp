90116d747cefb33ebd5ed07f5469382036bd28bf
Igor <68248740+richard96292@users.noreply.github.com>
2022-12-14 22:56:42 +0200

    Test with goerli

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+const { MNEMONIC, PROJECT_ID } = process.env;
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   contracts_build_directory: "./src/artifacts",
   networks: {
@@ -5,6 +10,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*",
+    },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://goerli.infura.io/v3/${PROJECT_ID}`
+        ),
+      network_id: 5,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
   mocha: {},
